6dcccbcfad48ab414f65796bacd1073361ef0512
Valcheez <avaldez@BOOK-79J43M7V1A>
2025-04-05 23:49:39 -0500

    Added a mock stats for test_invalid_player_entry

--- NBA_Stat_Comparison_App/tests/test_app.py
+++ NBA_Stat_Comparison_App/tests/test_app.py
@@ -1,6 +1,7 @@
 import pytest
 import nba_api
 from app import app
+from unittest.mock import patch
 
 @pytest.fixture
 def client():
@@ -19,13 +20,19 @@ def test_empty_form_submission(client):
     assert b'Player 1 season is required.' in response.data
     assert b'Player 2 season is required.' in response.data
 
-def test_invalid_player_entry(client):
+
+@patch('app.Player.get_stats')
+def test_invalid_player_entry(mock_get_stats, client):
+    # Simulate the first player not found (returns None), second one returns valid stats
+    mock_get_stats.side_effect = [None, {'Points': 30}]
+    
     response = client.post('/compare', data={
         'player1': 'Not A Real Player',
         'player1_season': '2023-24',
         'player2': 'Stephen Curry',
         'player2_season': '2023-24'
     }, follow_redirects=True)
+
     assert response.status_code == 200
     assert b'Could not retrieve stats for one or both players' in response.data
 
